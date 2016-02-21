921777925267c5991fca58ea9cc03e923e15f272
Panos Gr <nosgr94@gmail.com>
2016-02-21 23:23:01 +0100

    My first real commit I'm adding some of my own code

---
+++ src/Tree.java
@@ -0,0 +1,29 @@
+package src;
+
+/**
+ * Created by Panos on 21-Feb-16.
+ */
+public class Tree {
+
+
+    public String type;
+    public int height;
+
+    public Tree(int i) {
+        height = i;
+    }
+
+    public Tree(String h) {
+        type = h;
+    }
+
+
+    public Tree() {
+
+    }
+
+    public Tree(int i, String h) {
+        height = i;
+        type = h;
+    }
+}
--- src/main/App.java
+++ src/main/App.java
@@ -14,7 +14,15 @@ public class App {
         arboles[1] = new Tree("Roble");
         arboles[2] = new Tree();
         arboles[3] = new Tree(5,"Pino");
+
+
+        System.out.println("Un arbol de " +arboles[0].height +" metros" );
+        System.out.println("Un " +arboles[1].type );
+        System.out.println("Un arbol" );
+        System.out.println("Un " +arboles[3].type +" de " +arboles[3].height +" metros" );
+
     }
 
-   // System.out.println("Un arbol de" +arboles[0] );
+
+
 }
