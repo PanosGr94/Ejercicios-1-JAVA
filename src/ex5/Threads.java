8230770a565a988c4a825b52b74ca16d470fd505
Panos Gr <nosgr94@gmail.com>
2016-02-22 20:49:18 +0100

    4th exercise. Deleted some useless lines.

--- src/ex5/Threads.java
+++ src/ex5/Threads.java
@@ -7,12 +7,14 @@ public class Threads {
 
     public long startTime = System.currentTimeMillis();
 
-   public static void main(String args[]){
+   public static void main(String args[]) throws InterruptedException {
        Thread thread1 = new Thread(new runnableThread());
        Thread thread2 = new mySubThread();
        thread1.start();
        thread2.start();
 
+       thread1.join();
+       thread2.join();
        System.out.println("Se Acabo.");
    }
 }
